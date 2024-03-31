9aa5c5546b645d05d0819558247d44aa760531bc
MartinPC-uls <martin.pizarro@userena.cl>
2024-03-31 04:13:57 -0300

    implemented status.json

--- config.py
+++ config.py
@@ -1,8 +1,23 @@
+import json
 import yaml
+
 
 def read(file: str) -> dict:
     with open(file, "r") as f:
         data = yaml.safe_load(f)
 
     data["version"] = file.split("/")[-1].removesuffix(".yaml")
+    return data
+
+
+def save_status(status: dict, to: str):
+    json_string = json.dumps(status, indent=4)
+    with open(to, "w") as f:
+        f.write(json_string)
+
+
+def read_status(path: str) -> dict:
+    with open(path, "r") as f:
+        data = json.load(f)
+
     return data
--- trainer.py
+++ trainer.py
@@ -1,6 +1,8 @@
+import numpy as np
+
 from abc import ABC
-from accelerate import Accelerator
 from accelerate.utils import LoggerType, ProjectConfiguration
+from config import read, save_status, read_status
 
 
 class AcceleratorModule(ABC):
@@ -39,7 +41,7 @@ class Trainer:
                 checkpoint = "checkpoint1",
                 resume = False,
                 model_path: str = None,
-                enable_model_saving = True,
+                model_saving = "best_valid_loss",
                 enable_checkpointing = True,
                 checkpoint_every=1,
                 logging_dir = "logs",
@@ -50,7 +52,7 @@ class Trainer:
         self.checkpoint = checkpoint
         self.resume = resume
         self.model_path = model_path
-        self.enable_model_saving = enable_model_saving
+        self.model_saving = model_saving.lower()
         self.enable_checkpointing = enable_checkpointing
         self.checkpoint_every = checkpoint_every
         self.logging_dir = logging_dir
@@ -63,12 +65,10 @@ class Trainer:
             train_dataset,
             val_dataset = None
     ):
-        import numpy as np
         import os
         import torch
 
         from tqdm.auto import tqdm
-        from config import read
         from torch.utils.data import DataLoader
 
         model = getattr(module, "model", None)
@@ -84,7 +84,8 @@ class Trainer:
         if not self.model_path:
             self.model_path = cfg["version"]
 
-        os.makedirs(self.model_path, exist_ok=True)
+        if self.model_saving:
+            os.makedirs(self.model_path, exist_ok=True)
 
         train_dataloader = DataLoader(train_dataset, batch_size=hps["batch_size"], shuffle=True)
         val_dataloader = DataLoader(val_dataset, batch_size=hps["batch_size"], shuffle=True)
@@ -99,18 +100,24 @@ class Trainer:
         )
         self.accelerator.init_trackers(cfg["version"])
 
+        best_valid_loss = float("inf")
+        best_train_loss = float("inf")
+        status_epoch = 0
+
         if self.resume:
             if os.path.exists(self.checkpoint):
                 self.accelerator.load_state(self.checkpoint)
+                status = read_status(f"{self.checkpoint}/status.json")
+                best_valid_loss = status["best_valid_loss"]
+                best_train_loss = status["best_train_loss"]
+                status_epoch = status["epoch"]
             else:
                 print(f"{self.checkpoint} does not exist. Starting process from zero...")
 
         epochs = hps["epochs"]
         global_step = 0
         eval_step = len(train_dataloader) // len(val_dataloader)
-        best_valid_loss = float("inf")
-        best_train_loss = float("inf")
-        for epoch in range(epochs):
+        for epoch in range(status_epoch, epochs):
             eval_global_step = global_step
             model.train()
             train_losses = []
@@ -142,35 +149,57 @@ class Trainer:
 
                         eval_global_step += eval_step
 
-                if self.enable_model_saving:
-                    avg_valid_loss = np.mean(eval_losses)
-                    if avg_valid_loss < best_valid_loss:
-                        state_dict = self.accelerator.get_state_dict(model)
-                        unwrapped_model = self.accelerator.unwrap_model(model)
-                        unwrapped_model.save_pretrained(
-                            self.model_path,
-                            is_main_process=self.accelerator.is_main_process,
-                            state_dict=state_dict,
-                            max_shard_size="10GB",
-                            save_function=self.accelerator.save,
-                            safe_serialization=False # if True, some tensors will not be saved
-                        )
+                self._save_model_on_criteria(model, eval_losses, train_losses, best_valid_loss, best_train_loss)
             else:
-                if self.enable_model_saving:
+                if self.model_saving:
                     avg_train_loss = np.mean(train_losses)
                     if avg_train_loss < best_train_loss:
-                        state_dict = self.accelerator.get_state_dict(model)
-                        unwrapped_model = self.accelerator.unwrap_model(model)
-                        unwrapped_model.save_pretrained(
-                            self.model_path,
-                            is_main_process=self.accelerator.is_main_process,
-                            state_dict=state_dict,
-                            max_shard_size="10GB",
-                            save_function=self.accelerator.save,
-                            safe_serialization=False # if True, some tensors will not be saved
-                        )
+                        self._save_model(model, best_valid_loss, best_train_loss)
 
             if self.enable_checkpointing and epoch % self.checkpoint_every == 0:
                 self.accelerator.save_state(self.checkpoint)
+                save_status({
+                    "best_valid_loss": best_valid_loss,
+                    "best_train_loss": best_train_loss,
+                    "epoch": epoch
+                }, to=f"{self.checkpoint}/status.json")
 
         self.accelerator.end_training()
+
+    
+    def _save_model(self, model, best_valid_loss, best_train_loss):
+        state_dict = self.accelerator.get_state_dict(model)
+        unwrapped_model = self.accelerator.unwrap_model(model)
+        unwrapped_model.save_pretrained(
+            self.model_path,
+            is_main_process=self.accelerator.is_main_process,
+            state_dict=state_dict,
+            max_shard_size="10GB",
+            save_function=self.accelerator.save,
+            safe_serialization=False # if True, some tensors will not be saved
+        )
+
+        save_status({
+            "best_valid_loss": best_valid_loss,
+            "best_train_loss": best_train_loss,
+        }, to=f"{self.model_path}/status.json")
+
+    
+    def _save_model_on_criteria(self, model, eval_losses, train_losses, best_valid_loss, best_train_loss):
+        if self.model_saving is None:
+            return
+
+        saving_criteria = {
+            "best_valid_loss": (np.mean(eval_losses) < best_valid_loss),
+            "best_train_loss": (np.mean(train_losses) < best_train_loss),
+            "always": True
+        }
+
+        if self.model_saving in saving_criteria:
+            if saving_criteria[self.model_saving]:
+                self._save_model(model, best_valid_loss, best_train_loss)
+        else:
+            raise ValueError("Invalid type of model saving. Value must be: "
+                              "'best_valid_train_loss', "
+                              "'best_train_loss', or "
+                              "'always'.")
