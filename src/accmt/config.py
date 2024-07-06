f22ca5957b4ff2d6a03a4fb732ea70af22e9abd5
MartinPC-uls <martin.pizarro@userena.cl>
2024-07-06 18:09:09 -0400

    consider removing .yaml or .yml extensions

--- src/accmt/config.py
+++ src/accmt/config.py
@@ -6,7 +6,7 @@ def read(file: str) -> dict:
     with open(file, "r") as f:
         data = yaml.safe_load(f)
 
-    data["version"] = file.split("/")[-1].removesuffix(".yaml")
+    data["version"] = file.split("/")[-1].removesuffix(".yaml").removesuffix(".yml")
     return data
 
 
