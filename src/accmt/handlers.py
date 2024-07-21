7d726eb527ead0faeb5839067ad992762e7c7a68
MartinPC-uls <martin.pizarro@userena.cl>
2024-07-21 05:15:14 -0400

    added adecuated identation for dataclass

--- src/accmt/handlers.py
+++ src/accmt/handlers.py
@@ -2,6 +2,6 @@ from dataclasses import dataclass
 
 @dataclass
 class Handler:
-    KEYBOARD = 1
-    CUDA_OUT_OF_MEMORY = 2
-    ANY = 3
+    KEYBOARD            = 1
+    CUDA_OUT_OF_MEMORY  = 2
+    ANY                 = 3
