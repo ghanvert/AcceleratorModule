8625745c4f3772a54c0fac890d02e21a7dd90890
MartinPC-uls <martin.pizarro@userena.cl>
2024-07-24 02:16:54 -0400

    added documentation for Handler

--- src/accmt/handlers.py
+++ src/accmt/handlers.py
@@ -2,6 +2,13 @@ from dataclasses import dataclass
 
 @dataclass
 class Handler:
+    """
+    Properties:
+        `KEYBOARD`: Handles `KeyboardInterrupt` exceptions (CTRL + C).
+        `CUDA_OUT_OF_MEMORY`: Handles cuda out of memory errors derived from `RuntimeError` exceptions.
+        `ANY`: Handles any other exception not considered in these properties.
+        `ALL`: Handles all possible exceptions.
+    """
     KEYBOARD            = 1
     CUDA_OUT_OF_MEMORY  = 2
     ANY                 = 3
