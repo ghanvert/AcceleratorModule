092bf60a201da4c081549219a4c31c25433d5278
MartinPC-uls <martin.pizarro@userena.cl>
2024-08-17 02:38:50 -0400

    removed deprecated function

--- src/accmt/config.py
+++ src/accmt/config.py
@@ -2,12 +2,6 @@ import json
 import yaml
 
 
-def read(file: str) -> dict:
-    data = yaml.safe_load(open(file))
-    data["version"] = file.split("/")[-1].removesuffix(".yaml").removesuffix(".yml")
-    return data
-
-
 def save_status(status: dict, to: str):
     json_string = json.dumps(status, indent=4)
     open(to, "w").write(json_string)
