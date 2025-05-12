b7798b564c018463f14f6609b7c95d665f6a21d4
MartinPC-uls <martin.pizarro@userena.cl>
2025-05-11 21:50:11 -0400

    modified conf.py to avoid github pre-commit errors

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,17 @@
+# Copyright 2025 ghanvert. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -6,15 +20,16 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+
+
 project = "accmt"
 copyright = "2025, ghanvert"
 author = "ghanvert"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-import os
-import sys
 
 
 sys.path.insert(0, os.path.abspath("../.."))
